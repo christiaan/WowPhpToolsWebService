6e75db0e34428a728422079cf86a53dae7b61570
Christiaan Baartse <anotherhero@gmail.com>
2009-12-04 19:41:48 +0100

    Applied GPLv3 licence

---
+++ armory/urls_eu.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of WowPhpToolsWebService  <http://github.com/christiaan/WowPhpToolsWebService >
+ *
+ * WowPhpToolsWebService
+ * Copyright (C) 2009  Christiaan Baartse <christiaan@baartse.nl>
+ *
+ * WowPhpToolsWebService is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WowPhpToolsWebService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+return array(
+	'guildroster'	=> 'http://eu.wowarmory.com/guild-info.xml?r=%s&n=%s', // Realm Guild Page
+	'character'		=> 'http://eu.wowarmory.com/character-sheet.xml?r=%s&n=%s', // Realm Name
+);
